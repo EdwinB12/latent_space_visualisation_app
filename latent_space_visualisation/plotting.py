d933dedd740bb9d7ebf267ff926208a43a6c82d0
edwin <w.e.brown@sheffield.ac.uk>
2023-05-24 21:49:15 +0100

    Moved code into modules

--- latent_space_visualisation/plotting.py
+++ latent_space_visualisation/plotting.py
@@ -1,13 +1,19 @@
 import plotly.express as px
+import plotly.graph_objs as go
+import numpy as np
 
 
-def plot_latent_space():
-    pass
-
-
-def plot_input():
-    pass
-
-
-def plot_output():
-    pass
+def plot_image(arr, title):
+    return {
+        "data": [go.Heatmap(z=np.flip(arr, 0), colorscale="Viridis", showscale=False)],
+        "layout": {
+            "xaxis": {"showgrid": False, "zeroline": False, "visible": False},
+            "yaxis": {
+                "showgrid": False,
+                "zeroline": False,
+                "scaleanchor": "x",
+                "visible": False,
+            },
+            "title": title,
+        },
+    }
--- latent_space_visualisation/utils.py
+++ latent_space_visualisation/utils.py
@@ -1,10 +1,21 @@
+import dash_html_components as html
+import dash_core_components as dcc
+import dash
+from latent_space_visualisation import get_model, plotting, utils
+from dash.dependencies import Input, Output, State
+import numpy as np
 import tensorflow as tf
-from tensorflow import keras
+import plotly.express as px
+import plotly.graph_objs as go
+import pandas as pd
+from PIL import Image
 
 
-def get_latent_space(input_image, encoder, **kwargs):
-    return encoder.predict(input_image, **kwargs)
+def get_image_from_latent_space(ls, decoder_model):
+    return decoder_model.predict(ls)
 
 
-def get_model_output(decoder, latent_space, **kwargs):
-    return decoder.predict(latent_space, **kwargs)
+def img_arr_to_gif(img_arr, title, **kwargs):
+    imgs = [Image.fromarray(img) for img in img_arr]
+    imgs[0].save(f"tmp/{title}.gif", save_all=True, append_images=imgs[1:], **kwargs)
+    print("Gif Saved")
